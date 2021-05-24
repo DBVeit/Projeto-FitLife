654b5cb765bc3aebc570aaa363ee9638065f8c2c
DBVeit <davi.veit@pucpr.edu.br>
2021-05-23 21:59:29 -0300

    Atualização

---
+++ js/functions/fn_changeForm.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+    $("#aCriarConta").click(function (){
+       $("#formLogin").css("display", "none");
+       $("#formCadastro").css("display", "inline");
+    });
+});
