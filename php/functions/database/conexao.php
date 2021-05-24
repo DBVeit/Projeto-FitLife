6b961cadccd65f9baf86b2aaa9eb8059f4f0172b
DBVeit <43188547+DBVeit@users.noreply.github.com>
2021-05-24 09:03:48 -0300

    Update conexao.php

--- php/functions/database/conexao.php
+++ php/functions/database/conexao.php
@@ -9,14 +9,6 @@ $DB_USER = "root";
 $DB_PASS = "";
 $DB_SQUEMA = "project_fit-life";
 
-$connect = new mysqli($DB_SERVER, $DB_USER, $DB_PASS, $DB_SQUEMA);
+$mysqli = new mysqli($DB_SERVER, $DB_USER, $DB_PASS, $DB_SQUEMA);
 
-if ($connect->connect_error) {
-    die("Conexão com banco de dados falhou: " . $connect->connect_error);
-    $connect->close();
-}else{
-    $success = "Conexão bem-sucedida";
-    return $success;
-    $connect->close();
-}
 ?>
